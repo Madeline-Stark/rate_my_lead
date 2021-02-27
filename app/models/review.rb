932f68adfcebf8616773486dc5aeac9598776093
github username <github email address>
2021-02-27 15:01:34 -0800

    add presence validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
     belongs_to :teacher
     belongs_to :user
+
+    validates :text, :rating, presence: true
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,6 @@ class Teacher < ApplicationRecord
     belongs_to :creator, foreign_key: :user_id, class_name: "User", optional: true #optional means parent not required
     has_many :reviews
     has_many :critics, through: :reviews, source: :user
+
+    validates :name, presence: true
 end
