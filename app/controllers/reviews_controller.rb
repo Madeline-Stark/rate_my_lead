1498a9bbe462a6ebaa0a6302cbf6a68b55f3a2ff
github username <github email address>
2021-02-27 16:28:17 -0800

    built out create for reviews and added validation for numericality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,17 @@
 class ReviewsController < ApplicationController
   def new #nested
+    @teacher = Teacher.find_by(id: params[:teacher_id])
+    @review = @teacher.reviews.build
   end
 
   def create #nested
-
+    @teacher = Teacher.find_by(id: params[:teacher_id])
+    @review = current_user.reviews.build(review_params)
+    if @review.save
+      redirect_to review_path(@review)
+    else 
+      render :new 
+    end 
   end 
 
   def show
@@ -24,4 +32,10 @@ class ReviewsController < ApplicationController
   def destroy 
 
   end 
+
+  private
+
+  def review_params
+    params.require(:review).permit(:teacher_id, :rating, :text)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
     belongs_to :teacher
     belongs_to :user
 
-    validates :text, :rating, presence: true
+    validates :text, presence: true
+    validates :rating, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 5, message: "must be a number between 0 and 5"}
 end
